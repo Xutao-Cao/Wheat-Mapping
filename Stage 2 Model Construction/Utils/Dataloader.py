be6177ae5b7695be0168da835acc098682c24a5c
CaoXutao7 <1102797383@qq.com>
2022-04-23 08:37:21 -0500

    4.23

--- Stage 2 Model Construction/Utils/Dataloader.py
+++ Stage 2 Model Construction/Utils/Dataloader.py
@@ -30,6 +30,7 @@ def get_data(sites: list, years: list, base_path: str, type: str, verbose: bool)
     -------
     X, Y ->list
         X is a list of training images and Y is a list of corresponding labels.
+        image dim: [H, W, C, T] Where C is the channel for features and T is timestep
     """
     curr_path = None
     CDL_path = os.path.join(base_path, "CDL_label")
@@ -100,6 +101,73 @@ def ML_dataloader(sites: list, years: list, base_path = BASE_PATH, type = "Both"
     Y_stacked = Y_stacked.reshape((-1,1))
 
     return X_stacked, Y_stacked
+
+def Seq_model_dataloader(sites: list, years: list, base_path = BASE_PATH, type = "Both", verbose = FALSE):
+    """
+    Generate input for sequence models
+
+    Parameters
+    ----------
+    sites: list
+        contains the list of sites of images
+    
+    years: list
+        contains the list of years of images
+
+    base_path: str
+        the base path of the images
+
+    feature_type: str
+        "S1" , "L8" or "Both"
+    
+    verbose: bool
+        if true, print out the loading process
+    
+    Returns:
+    -------
+    X, Y ->ndarray
+        X is (N, L, C) shaped array where L is the length of the sequence , Y is a list of corresponding labels.
+    """
+    X_list , Y_list = get_data(sites, years, base_path, type, verbose)
+    X_stacked = np.stack(X_list, axis=0)
+    Y_stacked = np.stack(Y_list, axis=0)
+    X_stacked = X_stacked.reshape((-1, X_stacked.shape[3], X_stacked.shape[4]))
+    X_stacked = np.moveaxis(X_stacked, 3, -1)
+    Y_stacked = Y_stacked.reshape((-1,1))
+
+    return X_stacked, Y_stacked
+
+def Graph_model_dataloader(sites: list, years: list, base_path = BASE_PATH, type = "Both", verbose = FALSE):
+    """
+    Generate input for graph models
+
+    Parameters
+    ----------
+    sites: list
+        contains the list of sites of images
+    
+    years: list
+        contains the list of years of images
+
+    base_path: str
+        the base path of the images
+
+    feature_type: str
+        "S1" , "L8" or "Both"
+    
+    verbose: bool
+        if true, print out the loading process
+    
+    Returns:
+    -------
+    X, Y ->ndarray
+        X is (H,W)
+    """
+    X_list , Y_list = get_data(sites, years, base_path, type, verbose)
+    X_stacked = np.stack(X_list, axis=0)
+    Y_stacked = np.stack(Y_list, axis=0)
+    X_stacked = X_stacked.reshape((X_stacked.shape[0], X_stacked.shape[1], -1))
+    Y_stacked = Y_stacked.reshape((-1,1))
     
 
 
